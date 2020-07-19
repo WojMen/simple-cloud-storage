b45f712f89f633764bfa525875d49c554f086383
WojMen <xwojmenx@gmail.com>
2020-07-19 21:33:31 +0200

    add simple sql  Database

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-
 const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+db.run("CREATE TABLE users(name text, phone number)")
 
 const SECRET = "abc";
 
@@ -17,7 +21,7 @@ app.post("/register", (req, res) => {
   }
 
   const token = jwt.sign({ username: req.body.username }, SECRET);
-
+  db.run(`INSERT INTO users VALUES("${req.body.username}", "${req.body.validPhoneNumber})"`)
   res.send({
     token,
   });
